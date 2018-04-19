df79b2158e16123b80993a3623e6aeada4cf073d
Katniss <huangmin@MeenadeAir.fios-router.home>
2018-04-18 23:59:16 -0400

    Initial commit

---
+++ app/src/main/java/com/example/huangmin/takehomeassignment10_minh/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.huangmin.takehomeassignment10_minh;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.textViewSigup).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.textViewSigup:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+        }
+
+    }
+}
